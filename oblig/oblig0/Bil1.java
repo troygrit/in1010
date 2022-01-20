57080657233b60c70264bd9b084c020e9c590642
troygrit <98053860+troygrit@users.noreply.github.com>
2022-01-20 14:25:18 +0100

    Oppgave 1

--- oblig/oblig0/Bil1.java
+++ oblig/oblig0/Bil1.java
@@ -1,5 +1,9 @@
 package in1010.oblig.oblig0;
 
 class Bil1 {
+    public void BilHilsen() {
+        System.out.println("Jeg er en bil");
+
+    }
     
 }
--- oblig/oblig0/BilBruk1.java
+++ oblig/oblig0/BilBruk1.java
@@ -2,6 +2,8 @@ package in1010.oblig.oblig0;
 
 class BilBruk1 {
     public static void main(String[] args) {
+        Bil1 bil1 = new Bil1();
+        bil1.BilHilsen();
         
     }
 }
