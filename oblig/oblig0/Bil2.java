ef952304d6b5f81dd964eec8460f7e8937c57bcb
troygrit <98053860+troygrit@users.noreply.github.com>
2022-01-21 21:11:20 +0100

    Oppgave 2

--- oblig/oblig0/Bil2.java
+++ oblig/oblig0/Bil2.java
@@ -1,5 +1,12 @@
 package in1010.oblig.oblig0;
 
-class Bil2 {
+public class Bil2 {
+
+    public String bilnummer;
+
+    public void Bilnummer(String nummer) {
+        bilnummer = nummer;
+        System.out.println("Bilnummer: " + bilnummer);
+    };
     
 }
--- oblig/oblig0/BilBruk2.java
+++ oblig/oblig0/BilBruk2.java
@@ -2,6 +2,8 @@ package in1010.oblig.oblig0;
 
 class BilBruk2 {
     public static void main(String[] args) {
+        Bil2 bil2 = new Bil2();
+        bil2.Bilnummer("AA998877");
         
     }
 }
