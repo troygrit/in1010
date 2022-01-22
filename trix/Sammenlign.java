28d24ca2ebe9950c6a5dc6cf53d401a63b000f8c
troygrit <98053860+troygrit@users.noreply.github.com>
2022-01-22 11:56:22 +0100

    Trix 01.06 Sammenligne to tall

---
+++ trix/Sammenlign.java
@@ -0,0 +1,20 @@
+package in1010.trix;
+
+public class Sammenlign {
+
+    public static void main(String[] args) {
+        
+        int a = 11;
+        int b = 7;
+    
+        
+        if(a>b) {
+            System.out.println(a + " er storre enn " + b);
+        }
+        else {
+            System.out.println(a + " er ikke storre enn " + b);
+        }
+        
+    }
+    
+}
